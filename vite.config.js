bc9134d63c481005e89ea6542de554e877a7d91a
Praveen Voruganti <praveenvoruganti@Praveens-MacBook-Pro.local>
2026-02-13 08:41:26 +0530

    feat: add GitHub Pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/sainath.com/',
   plugins: [react()],
   server: {
     port: 3000,
